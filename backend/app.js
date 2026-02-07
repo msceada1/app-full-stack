5989057ef4aca36620c56c9dc4b3ab779c481ebf
Alberto Carmona Oller <carmonaolleralberto@gmail.com>
2026-02-07 23:52:44 +0100

    Cambios en app

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,10 @@ import express from 'express';
 import { connectDB } from './src/config/db.js';
 import seedRoutes from './src/routes/seedRoutes.js';
 
+// Importamos las nuevas rutas
+import cocheRoutes from './src/routes/cocheRoutes.js';
+import ventaRoutes from './src/routes/ventaRoutes.js';
+
 // 1. Cargamos las variables de entorno
 dotenv.config();
 
@@ -18,18 +22,19 @@ app.use(express.json()); // Para que el servidor entienda JSON
 // Intentará leer el puerto del archivo .env, si no existe usa el 3000
 const PORT = process.env.PORT || 3000;
 
-// 5. Rutas (Las activaremos cuando creemos los archivos en /src/routes)
-// app.use('/api/coches', cocheRoutes);
-
+// 5. Rutas
+// Ruta base de bienvenida
 app.get('/', (req, res) => {
     res.send('¡Bienvenido a la API del Concesionario!');
 });
 
-// Usar las rutas
-app.use('/api/admin', seedRoutes);
+// Rutas de la API
+app.use('/api/admin', seedRoutes);   // Semillas
+app.use('/api/coches', cocheRoutes); // Inventario y Búsquedas (Puntos 2.1 y 2.2)
+app.use('/api/ventas', ventaRoutes); // Transacciones e Historial (Puntos 2.2 y 2.3)
 
 // 6. Inicio del Servidor
 app.listen(PORT, () => {
-    console.log(`🚗 API del Concesionario funcionando en http://localhost:${PORT}`);
-    console.log(`🚀 Presiona Ctrl+C para detener el servidor`);
+    console.log('🚗 API del Concesionario funcionando en http://localhost:${PORT}');
+    console.log('🚀 Presiona Ctrl+C para detener el servidor');
 });
