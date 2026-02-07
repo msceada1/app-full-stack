2a8c37c38ed59f9a0f858542656b3569858e851a
Alberto Carmona Oller <carmonaolleralberto@gmail.com>
2026-02-07 22:22:30 +0100

    Importaciones

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import Inventory from './components/Inventory'
+import Buscador from './components/Buscador'
+import ModalRegistroVenta from './components/ModalRegistroVenta'
 
 function App() {
   return (
     <div className="min-h-screen bg-blue-500 p-8 flex flex-col items-center">
       // El inventario de coches, que es el componente que hemos creado
       <Inventory />
+      <Buscador />
+      <ModalRegistroVenta />
 
     </div>
   )
